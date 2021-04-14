5be6cf99bfa1f8fe82e97c744066fbdd2b4d96aa
Lúcio Nonato <39486464+N0N4T0@users.noreply.github.com>
2021-04-14 01:36:54 -0300

    3 commit
    reestruturação completa de layout, quase pronto

---
+++ javascript/pilha.js
@@ -0,0 +1,22 @@
+// $(document).ready(function() { 
+//     $("div#pilha div").css({
+//      "background-color" : "red",
+//      "height" : "50px",
+//      "width" : "50px"
+//     });
+//     $("div#bloco div:first-child").css("margin-top","10px")
+ 
+//     a();
+//  });
+ 
+//  function a (){
+//     var m =  $("div#bloco div:first-child").css("margin-top");  
+//     $('div#pilha div').each(function(i){
+ 
+//        var atual = parseInt($(this).css("margin-top"));
+//        var proximo = $(this).next();
+       
+//        proximo.css("margin-top", atual+46+"px");
+//        console.log("div: "+ i + " " + $(this).css("margin-top"))
+//     });
+//  }
